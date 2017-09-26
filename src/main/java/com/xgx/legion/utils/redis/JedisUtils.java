f008d540e7941c1e461633de40238ba6ff09d183
xgx-t134 <xgx-t134@XGX-T134.dinpay.net>
2017-09-26 14:35:11 +0800

    update

---
+++ src/main/java/com/xgx/legion/utils/redis/JedisUtils.java
@@ -0,0 +1,7 @@
+package com.xgx.legion.utils.redis;
+
+public class JedisUtils {
+	public static void main(String[] args) {
+		
+	}
+}
--- src/main/java/com/xgx/legion/utils/redis/RedisUtils.java
+++
@@ -1,5 +0,0 @@
-package com.xgx.legion.utils.redis;
-
-public class RedisUtils {
-
-}
